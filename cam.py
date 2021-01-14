7fbc1b86baa8a9dc41921429f344bc73b5a72b63
Sagar Solanki <77234764+iMquTiPy@users.noreply.github.com>
2021-01-14 16:11:28 +0530

    Create cam.py

---
+++ cam.py
@@ -0,0 +1,25 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to open Camera please try again")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape key pressed, closing camera.")
+        break
+    
+
+cam.release()
+
+cv2.destroyAllWindows()
